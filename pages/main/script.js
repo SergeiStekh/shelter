421ff66e578e40b31c95e739fc7ff95e0a05b8b1
SergeyCodeJs <sergeysteh@gmail.com>
2022-04-27 13:50:52 +0200

    fix: links to images in script file

--- pages/main/script.js
+++ pages/main/script.js
@@ -210,7 +210,7 @@ function navigation() {
 function modal() {
   let animalsData = {
     jennifer: {
-      url: "/assets/images/pets-jennifer.jpg",
+      url: "../../assets/images/pets-jennifer.jpg",
       name: "Jennifer",
       breed: "Dog - Labrador",
       description: "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
@@ -220,7 +220,7 @@ function modal() {
       parasites: "none"
     },
     katrine: {
-      url: "/assets/images/pets-katrine-third.jpg",
+      url: "../../assets/images/pets-katrine-third.jpg",
       name: "Katrine",
       breed: "Cat - British Shorthair",
       description: "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
@@ -230,7 +230,7 @@ function modal() {
       parasites: "none"
     },
     sophia: {
-      url: "/assets/images/pets-katrine-first.jpg",
+      url: "../../assets/images/pets-katrine-first.jpg",
       name: "Sophia",
       breed: "Dog - Shih tzu",
       description: "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
@@ -240,7 +240,7 @@ function modal() {
       parasites: "none"
     },
     woody: {
-      url: "/assets/images/pets-woody.jpg",
+      url: "../../assets/images/pets-woody.jpg",
       name: "Woody",
       breed: "Dog - Golden Retriever",
       description: "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
@@ -250,7 +250,7 @@ function modal() {
       parasites: "none"
     },
     scarlett: {
-      url: "/assets/images/pets-scarlet.jpg",
+      url: "../../assets/images/pets-scarlet.jpg",
       name: "Scarlett",
       breed: "Dog - Jack Russell Terrier",
       description: "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
@@ -260,7 +260,7 @@ function modal() {
       parasites: "none"
     },
     timmy: {
-      url: "/assets/images/pets-timmy.jpg",
+      url: "../../assets/images/pets-timmy.jpg",
       name: "Timmy",
       breed: "Cat - British Shorthair",
       description: "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
@@ -270,7 +270,7 @@ function modal() {
       parasites: "none"
     },
     freddie: {
-      url: "/assets/images/pets-katrine-second.jpg",
+      url: "../../assets/images/pets-katrine-second.jpg",
       name: "Freddie",
       breed: "Cat - British Shorthair",
       description: "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
@@ -280,7 +280,7 @@ function modal() {
       parasites: "none"
     },
     charly: {
-      url: "/assets/images/pets-charly.jpg",
+      url: "../../assets/images/pets-charly.jpg",
       name: "Charly",
       breed: "Dog - Jack Russell Terrier",
       description: "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
